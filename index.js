959b934e854ed5a060c826063dc41a7ca2e29763
shehzad1212 <tawhidulislam261@gmail.com>
2022-11-25 18:18:52 +0600

    basic setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+const categories = require("./data/categories.json");
+
+// categories api call
+app.get("/", (req, res) => {
+  res.send("new api run");
+});
+
+// product api call
+app.get("/books-categories", (req, res) => {
+  res.send(categories);
+});
+
+app.listen(port, () => {
+  console.log("boook sop");
+});
