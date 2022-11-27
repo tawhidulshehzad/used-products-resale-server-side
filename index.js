1fddb80a178ff6d9e71ed532e5fcd763af450c63
shehzad1212 <tawhidulislam261@gmail.com>
2022-11-28 03:05:53 +0600

    now route for users

--- index.js
+++ index.js
@@ -47,6 +47,13 @@ async function run() {
         res.send(products);
       }
     });
+    // bookings get
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
+    });
 
     // booking post
 
